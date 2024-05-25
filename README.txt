b9bc1c6a5124423e74776b03d83774ea016db9e5
Muhammad Saad <muhammed.saadi99@gmail.com>
2024-05-25 23:05:17 +0500

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+#### Mini Calculator ####
+to run this Calculator, please follow the following steps.
+
+- npm install
+- npx mini-calculator
---
+++ bin/index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import { sum, subtract, multiply, division } from "./operations.js";
+import inquirer from "inquirer";
+
+const calculation = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter your 1st Number: ",
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter your 2nd Number: ",
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Select Operation ",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+
+if (calculation.operator == "+") {
+    sum(calculation.num1, calculation.num2);
+} else if (calculation.operator == "-") {
+    subtract(calculation.num1, calculation.num2);
+} else if (calculation.operator == "*") {
+    multiply(calculation.num1, calculation.num2);
+} else if (calculation.operator == "/") {
+    division(calculation.num1, calculation.num2);
+} else {
+    console.log("Invalid Operation 😵‍💫");
+}
